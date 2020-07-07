caf5e0038afc61e255778d53484eb2783b54f687
Ashish Amar <ashishamar1999@gmail.com>
2020-07-07 22:42:44 +0530

    Update RecycleBinSize.py

--- RecycleBinSize.py
+++ RecycleBinSize.py
@@ -1,15 +1,7 @@
 import os
 os.chdir("C:\\")
 print("CWD = "+os.getcwd())
-a = os.popen('dir /s "$Recycle.Bin"').read().split("Total Files Listed:\n")[1].split("(s)")[1].split("bytes")[0].strip()
-a = float("".join(a.split(",")))
-print(f"{a} in bytes\n{a/1024} in Kb\n{a/(1024*1024)} in Mb\n{a/(1024*1024*1024)} in Gb")
+bin_size = os.popen('dir /s "$Recycle.Bin"').read().split("Total Files Listed:\n")[1].split("(s)")[1].split("bytes")[0].strip()
+bin_size = float("".join(bin_size.split(",")))
+print(f"{bin_size} in bytes\n{bin_size/1024} in Kb\n{bin_size/(1024*1024)} in Mb\n{bin_size/(1024*1024*1024)} in Gb")
 input("Press Any Key To Exit...")
-"""
-https://stackoverflow.com/questions/12813826/get-folder-size-from-windows-command-line
-...However, this has several problems because cmd is limited to 32-bit signed integer arithmetic. 
-So it will get sizes above 2 GiB wrong...
-https://www.daniweb.com/programming/software-development/code/216951/size-of-a-file-folder-directory-python
-https://stackoverflow.com/questions/483864/windows-command-for-file-size-only
-https://weblogs.asp.net/jongalloway/top-10-dos-batch-tips-yes-dos-batch
-"""
